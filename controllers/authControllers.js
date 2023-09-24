e48bbaaa2419fa745e53388989dd02dd3b7d0eff
Mohamed-Elbehery <dev.elbehery@gmail.com>
2023-09-24 15:07:20 +0300

    register auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const User = require("../models/auth");
+const jwt = require("jsonwebtoken")
+const maxAge = 3 * 24 * 60 * 60;
 
 // Handling Errors
 const handleErrors = (err) => {
@@ -25,12 +27,30 @@ const signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { maxAge: maxAge * 1000});
+    res.status(201).json({ user: user._id, token });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json(errors);
   }
 };
+
+const createToken = (id) => {
+  /*
+    todo =>    jwt.sign({
+    todo =>      payload: value,
+    todo =>      secret: value,
+    todo =>      options: value,
+    todo =>    })
+
+    !     this will return the signature token
+  */
+
+  return jwt.sign({ id }, 'mySecretKey', {
+    expiresIn: maxAge
+  })
+}
 
 const login_post = async (req, res) => {};
 
